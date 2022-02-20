34a1471099b6b19c751f1d9ece9085b4e9df7859
sungchulyun <proattacker@naver.com>
2022-02-20 21:32:05 +0900

    게시판 만들기 리스트까지

---
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -0,0 +1,41 @@
+package com.example.board.controller;
+
+import com.example.board.entity.Board;
+import com.example.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board/write")
+    public String boardWriteForm(){
+        return "boardwrite";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board){
+        boardService.write(board);
+
+        return "";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model){
+
+        model.addAttribute("list", boardService.boardList());
+
+        return "/boardlist";
+    }
+    @GetMapping("/board/view")
+    public String boardView(){
+        return "boardview";
+    }
+}

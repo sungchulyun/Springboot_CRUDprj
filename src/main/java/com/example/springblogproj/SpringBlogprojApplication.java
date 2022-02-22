296e1d544a29add0557254248599158bb3aceed9
sungchulyun <proattacker@naver.com>
2022-02-22 11:49:34 +0900

    토이프로젝트 세팅

---
+++ src/main/java/com/example/springblogproj/SpringBlogprojApplication.java
@@ -0,0 +1,13 @@
+package com.example.springblogproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBlogprojApplication.class, args);
+	}
+
+}
